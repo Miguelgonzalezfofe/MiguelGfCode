f2e5cc577295603b95947a739e9de7d12018a9b7
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2024-08-24 11:45:31 -0300

    creacion del css y js, junto a secciones del html como header, sobre mi y proyectos

---
+++ main.js
@@ -0,0 +1,23 @@
+// JavaScript para efectos de scroll en los enlaces
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('section');
+    const navLinks = document.querySelectorAll('nav ul li a');
+
+    let currentSection = '';
+
+    sections.forEach(section => {
+        const sectionTop = section.offsetTop - 60;
+        const sectionHeight = section.clientHeight;
+
+        if (scrollY >= sectionTop && scrollY <= sectionTop + sectionHeight) {
+            currentSection = section.getAttribute('id');
+        }
+    });
+
+    navLinks.forEach(link => {
+        link.classList.remove('active');
+        if (link.getAttribute('href').includes(currentSection)) {
+            link.classList.add('active');
+        }
+    });
+});
