10a8503bcf3b77263c48a368b98138ef143f7183
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2024-08-25 02:39:21 -0300

    mejor adaptasion a dispositivos, efectos y botones

--- main.js
+++ main.js
@@ -21,3 +21,38 @@ window.addEventListener('scroll', () => {
         }
     });
 });
+
+
+document.addEventListener("DOMContentLoaded", function () {
+    const frases = [" JavaScript ", " React ", " Paginas Web "];
+    let i = 0;
+    let j = 0;
+    let actual = "";
+    let isDeleting = false;
+    const textoElement = document.getElementById("escribir");
+
+    function escribir() {
+        const palabraCompleta = frases[i];
+
+        if (isDeleting) {
+            actual = palabraCompleta.substring(0, j--);
+        } else {
+            actual = palabraCompleta.substring(0, j++);
+        }
+
+        textoElement.textContent = actual;
+
+        if (!isDeleting && j === palabraCompleta.length) {
+            isDeleting = true;
+            setTimeout(escribir, 1500); // Pausa al final de cada frase
+        } else if (isDeleting && j === 0) {
+            isDeleting = false;
+            i = (i + 1) % frases.length;
+            setTimeout(escribir, 800); // Pausa antes de comenzar a escribir la nueva frase
+        } else {
+            setTimeout(escribir, isDeleting ? 70 : 80); // Velocidad de escritura y borrado
+        }
+    }
+    escribir();
+});
+
