047712cdcd87700b8da3e6a5fcacbbe4241f5b28
Miguel Eduardo Gonzalez Fofe <247mmgg@gmail.com>
2024-08-28 05:46:09 -0300

    terminado el cambio de idioma del contenido hasta ahora, arreglo dde la vista de los link de los proyectos

--- main.js
+++ main.js
@@ -24,6 +24,7 @@ window.addEventListener('scroll', () => {
 
 
 document.addEventListener("DOMContentLoaded", function () {
+
     const frases = [" JavaScript ", " React.js ", " Web "];
     let i = 0;
     let j = 0;
@@ -57,11 +58,11 @@ document.addEventListener("DOMContentLoaded", function () {
     escribir();
 });
 
-document.getElementById('toggle-dark-mode').addEventListener('click', function() {
+document.getElementById('toggle-dark-mode').addEventListener('click', function () {
     document.body.classList.toggle('dark-mode');
-    
+
     // Cambiar el ícono del botón según el modo
-    if(document.body.classList.contains('dark-mode')) {
+    if (document.body.classList.contains('dark-mode')) {
         this.textContent = '☀️'; // Ícono de sol para modo claro
     } else {
         this.textContent = '🌙'; // Ícono de luna para modo oscuro
@@ -72,41 +73,34 @@ document.getElementById('languageSelector').addEventListener('change', changeLan
 
 function changeLanguage() {
     const language = document.getElementById('languageSelector').value;
-    
-    fetch(`./idioma-${language}.json`)
-    .then(response => response.json())
-    .then(data => {
-        
-        
-        // Cambiar texto en el header
-        document.getElementById('inicio').textContent = data.header.inicio;
-        document.getElementById('proyecto').textContent = data.header.proyectos;
-        document.getElementById('sobre-mi').textContent = data.header.sobre_mi;
-        document.getElementById('contacto').textContent = data.header.contacto;
-        // document.getElementById('toggle-dark-mode').textContent = data.header.toggle_dark_mode;
-        
-        // // Cambiar texto en la sección hero
-        // document.getElementById('hero-title').textContent = data.hero.titulo;
-        // document.getElementById('escribir').textContent = data.hero.subtitulo;
-        // document.getElementById('hero-description').textContent = data.hero.descripcion;
-        // document.getElementById('boton-proyectos').textContent = data.hero.boton_proyectos;
-        
-        // // Cambiar texto en la sección de proyectos
-        // document.getElementById('proyectos-title').textContent = data.proyectos.titulo;
-        // document.getElementById('proyecto1-titulo').textContent = data.proyectos.proyecto1.titulo;
-        // document.getElementById('proyecto1-descripcion').textContent = data.proyectos.proyecto1.descripcion;
-        // document.getElementById('proyecto1-github').textContent = data.proyectos.proyecto1.github;
-        // document.getElementById('proyecto1-link').textContent = data.proyectos.proyecto1.link_proyecto;
 
-        // document.getElementById('proyecto2-titulo').textContent = data.proyectos.proyecto2.titulo;
-        // document.getElementById('proyecto2-descripcion').textContent = data.proyectos.proyecto2.descripcion;
-        // document.getElementById('proyecto2-github').textContent = data.proyectos.proyecto2.github;
-        // document.getElementById('proyecto2-link').textContent = data.proyectos.proyecto2.link_proyecto;
-        
-        // // Cambiar texto en la sección sobre mí
-        // document.getElementById('sobre-mi-title').textContent = data.sobre_mi.titulo;
-    })
-    .catch(error => console.error('Error al cargar el archivo JSON:', error));
+    fetch(`./idioma-${language}.json`)
+        .then(response => response.json())
+        .then(data => {
+
+
+            // Cambiar texto en el header
+            document.getElementById('header-inicio').textContent = data.header.inicio;
+            document.getElementById('header-proyecto').textContent = data.header.proyectos;
+            document.getElementById('header-sobre-mi').textContent = data.header.sobre_mi;
+            document.getElementById('header-contacto').textContent = data.header.contacto;
+
+            // Cambiar texto en la sección hero
+            // document.getElementById('destacar').textContent = data.hero.subtitulo;
+            document.getElementById('hero-description').textContent = data.hero.descripcion;
+            document.getElementById('boton-proyectos').textContent = data.hero.boton_proyectos;
+
+            // // Cambiar texto en la sección de proyectos
+            document.getElementById('proyectos-title').textContent = data.proyectos.titulo;
+            document.getElementById('proyecto1-descripcion').textContent = data.proyectos.proyecto1.descripcion;
+
+            // demas proyectos
+
+
+            // // Cambiar texto en la sección sobre mí
+            document.getElementById('sobre-mi-title').textContent = data.sobre_mi.titulo;
+        })
+        .catch(error => console.error('Error al cargar el archivo JSON:', error));
 }
 
 // Inicializar con el idioma por defecto
